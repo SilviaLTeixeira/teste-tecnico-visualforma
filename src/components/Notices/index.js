81ea94b7d06c35d80ba7d80465c1b0959e83d898
silvia_teixeira <quimicasilvia20@gmail.com>
2022-10-04 18:05:37 -0300

    Organizando arquivos

---
+++ src/components/Notices/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./Notices.module.css";
+
+const Notices = () => {
+    return (  
+        <div className={styles.}></div>
+    );
+}
+ 
+export default Notices;
